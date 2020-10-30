0d8dbc3718775d0fc377e2e85a81e4f7e9e59388
Sarah Iraurgui <iraurguisj@gmail.com>
2020-10-30 17:03:25 -0400

    deplyoing to heroku 2

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 // routes
