6fbd0f6080d5f2ccb43585045c7d877085c69988
Sarah Iraurgui <iraurguisj@gmail.com>
2020-10-29 21:10:53 -0400

    routes getting exported to fix errors

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
+
+
+module.exports = function(app){
+
  router.post("/api/workouts", (req, res) => {
      Workout.create({})
  });
+};
--- routes/view.js
+++ routes/view.js
@@ -1,6 +1,9 @@
+module.exports = function(app){
+
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
   router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
+};
