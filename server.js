982ede5abc97a24f951b11a76c653c6ef7ea7462
Sarah Iraurgui <iraurguisj@gmail.com>
2020-10-30 17:40:04 -0400

    delpying 4

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const PORT = 3000;
 const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/workout", {
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
+
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Our app is running on port ${PORT}`);
 });
