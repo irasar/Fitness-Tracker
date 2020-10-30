0101a0dfdd2196c3c5620e1149361dd40d711c20
Sarah Iraurgui <iraurguisj@gmail.com>
2020-10-29 22:59:42 -0400

    get route range created

--- routes/api.js
+++ routes/api.js
@@ -23,6 +23,18 @@ router.post("/api/workouts", (req, res) => {
         });
 });
 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+        .then(dbWorkouts => {
+            console.log(dbWorkouts)
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
 
 
 module.exports = router;
