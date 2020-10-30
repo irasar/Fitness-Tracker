86e919057509b22c31f007798962df2322c433b0
Sarah Iraurgui <iraurguisj@gmail.com>
2020-10-29 23:46:16 -0400

    put request working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Fitness-Tracker
+
+
+
+
--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,37 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-
-router.get("/api/workouts", (req, res) => {
-    Workout.find()
-        .then((dbWorkout) => {
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
             res.json(dbWorkout);
         })
-        .catch((err) => {
+        .catch(err => {
             res.json(err);
         });
 });
 
-
-router.post("/api/workouts", (req, res) => {
-    Workout.create({})
-        .then((dbWorkout) => {
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        //"runvalidators" will ensure new exercises meet our schema requirments
+        { new: true, runValidators: true }
+    )
+        .then(dbWorkout => {
             res.json(dbWorkout);
         })
-        .catch((err) => {
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
             res.json(err);
         });
 });
@@ -26,8 +39,8 @@ router.post("/api/workouts", (req, res) => {
 router.get("/api/workouts/range", (req, res) => {
     Workout.find({}).limit(7)
         .then(dbWorkouts => {
-            console.log(dbWorkout)
-            res.json(dbWorkout);
+            console.log(dbWorkouts)
+            res.json(dbWorkouts);
         })
         .catch(err => {
             res.json(err);
